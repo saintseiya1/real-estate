cede9001f1048268fb6d93772b12ac88cf20f062
Roy Janechuti <roysahn@hotmail.com>
2022-03-29 22:25:13 -0700

    Show all listings plus pagination

--- app/Http/Controllers/Admin/ListingController.php
+++ app/Http/Controllers/Admin/ListingController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Listing;
 use App\Helper\Helper;
+use Illuminate\Support\Facades\DB;
 
 class ListingController extends Controller
 {
@@ -16,7 +17,8 @@ class ListingController extends Controller
      */
     public function index()
     {
-        return view('admin/listings/index');
+        $listings = Listing::paginate(7);
+        return view('admin/listings/index', ['listings' => $listings]);
     }
 
     /**
--- resources/views/admin/listings/index.blade.php
+++ resources/views/admin/listings/index.blade.php
@@ -3,5 +3,39 @@
 @section('page-title', 'Show All Listings')
 
 @section('content')
+<div id="mainContent">
     <h1>Show All Listings</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="bgc-white bd bdrs-3 p-20 mB-20">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($listings as $listing)
+                        <tr>
+                            <th scope="row">{{ $listing->id }}</th>
+                            <td><a href="{{ route('admin/listings/edit', [
+                                'slug' => $listing->slug,
+                                'id' => $listing->id
+                                ]) }}">
+                                    {{ $listing->address }} {{ $listing->address2}}<br>
+                                    {{ $listing->city}}, {{ $listing->state}} {{ $listing->zipcode}}
+                                </a>
+                            </td>
+                            <td>Active</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $listings->links() }}
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
